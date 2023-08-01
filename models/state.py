0fec87d2039ccfb680aff18bee1668c6deda79c8
KrasniKot <6187@holbertonstudents.com>
2023-07-31 21:25:15 -0700

    city, state, basemodel now implementing base

--- models/state.py
+++ models/state.py
@@ -1,8 +1,25 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models import storage
+from models.base_model import BaseModel, Base
+from models.city import City
+from os import getenv
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.orm import relationship
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = "states"
+    name = Column(String(60), nullable=False)
+    cities = relationship("City",  backref="state", cascade="delete")
+
+    if getenv("HBNB_TYPE_STORAGE") != "db":
+        @property
+        def cities(self):
+            """Getter method"""
+            listOfCities = []
+            for city in storage.all(City).values():
+                if city.state_id == self.id:
+                    listOfCities.append(city)
+            return listOfCities
