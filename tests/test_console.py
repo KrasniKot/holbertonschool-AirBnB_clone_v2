e615a2b0511c16db22d139953d45963d61d24e0c
KrasniKot <6187@holbertonstudents.com>
2023-08-04 06:56:34 -0700

    console test file created

---
+++ tests/test_console.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""This module conatins test for console"""
+
+import unittest
+
+
+class testConsole(unittest.TestCase):
+    """Defines the tests"""
+    pass
+
+
+if __name__ == "__main__":
+    unittest.main()
