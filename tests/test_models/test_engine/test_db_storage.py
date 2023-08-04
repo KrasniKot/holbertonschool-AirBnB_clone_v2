7d09b05660339fdb9d951979a72023c761674556
KrasniKot <6187@holbertonstudents.com>
2023-08-03 21:53:48 -0700

    more unittests added

---
+++ tests/test_models/test_engine/test_db_storage.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Contains tests for DBStorage"""
+
+import unittest
+from models.engine.db_storage import DBStorage
+from os import getenv
+
+
+class TestDBStorage(unittest.TestCase):
+    """Defines the tests for DBStorage"""
+
+    @unittest.skipIf(getenv == ("HBNB_TYPE_STORAGE") == "file", "Won't pass")
+
+    def test_ItHasAttr(self):
+        """Tests for the attributes"""
+        self.assertTrue(hasattr(DBStorage, "_DBStorage__engine"))
+        self.assertTrue(hasattr(DBStorage, "_DBStorage__session"))
+        self.assertTrue(hasattr(DBStorage, "all"))
+        self.assertTrue(hasattr(DBStorage, "new"))
+        self.assertTrue(hasattr(DBStorage, "save"))
+        self.assertTrue(hasattr(DBStorage, "delete"))
+        self.assertTrue(hasattr(DBStorage, "reload"))
+
+
+if __name__ == "__main__":
+    unittest.main()
