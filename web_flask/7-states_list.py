fe3123ae7c33e5cd95af6de460d4bd67095da7f8
KrasniKot <6187@holbertonstudents.com>
2023-08-16 07:52:25 -0700

    task 8 doc up

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,72 +1,26 @@
 #!/usr/bin/python3
+"""Starts a Flask web application.
 """
-Starts a Flask web app
-"""
-
+from models import storage
 from flask import Flask, render_template
-from models import storage, State
 
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 
-@app.route("/")
-def hello_world():
-    """Displays 'Hello HBNB!'"""
-    return "Hello HBNB!"
+@app.route("/states_list", strict_slashes=False)
+def states_list():
+    """Displays an HTML page with a list of all States
 
-
-@app.route("/hbnb")
-def hbnb():
-    """Displays 'HBNB!'"""
-    return "HBNB"
-
-
-@app.route("/c/<text>")
-def cText(text):
-    """Returns a text"""
-    text = text.replace("_", " ")
-    return f"C {text}"
-
-
-@app.route("/python")
-@app.route("/python/<text>")
-def pythonText(text="is cool"):
-    """Returns a text"""
-    text = text.replace("_", " ")
-    return f"Python {text}"
-
-
-@app.route("/number/<int:n>")
-def number(n):
-    """Returns only if n is an integer"""
-    return f"{n} is a number"
-
-
-@app.route("/number_template/<int:n>")
-def numberTemplate(n):
-    """Returns an html page"""
-    return render_template("5-number.html", n=n)
-
-
-@app.route("/number_odd_or_even/<int:n>")
-def evenSo(n):
-    """Returns an html page depending on the parity of n"""
-    return render_template("6-number_odd_or_even.html", n=n)
+    ..sorted by name.
+    """
+    states = storage.all("State")
+    return render_template("7-states_list.html", states=states)
 
 
 @app.teardown_appcontext
-def closing(var=None):
-    """Ends the current session"""
+def teardown(args):
+    """Ends the current session."""
     storage.close()
-
-
-@app.route("/states_list")
-def stateList():
-    """Returns an html page displaying a list of states"""
-    return render_template(
-                    "7-states_list.html",
-                    statess=storage.all(State).values())
 
 
 if __name__ == "__main__":
