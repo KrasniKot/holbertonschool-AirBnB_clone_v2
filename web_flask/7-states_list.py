354eefecdfa3551e06ba36a7c098fd90e4b48a43
KrasniKot <6187@holbertonstudents.com>
2023-08-16 13:01:33 -0700

    task8 redo

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,1 +1,28 @@
+#!/usr/bin/python3
+"""Starts a Flask web application.
+"""
+from models import storage
+from models.state import State
+from flask import Flask, render_template
 
+app = Flask(__name__)
+
+
+@app.route("/states_list", strict_slashes=False)
+def statesList():
+    """Displays an HTML page with a list of all States
+
+    sorted by name.
+    """
+    return render_template("7-states_list.html",
+                           states=storage.all(State).values())
+
+
+@app.teardown_appcontext
+def tearDown(self):
+    """Ends the current session."""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
