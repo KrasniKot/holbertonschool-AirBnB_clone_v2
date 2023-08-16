1625f961d1989a24ac8e23e6f04327012642048f
KrasniKot <6187@holbertonstudents.com>
2023-08-16 08:20:50 -0700

    import fixed 8

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """Starts a Flask web application.
 """
-from models import storage, State
+from models import storage
+from models.state import State
 from flask import Flask, render_template
 
 app = Flask(__name__)
