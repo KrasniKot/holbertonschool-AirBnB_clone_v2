6483d722ad5a6e39106be9e2c6a04208367b6397
KrasniKot <6187@holbertonstudents.com>
2023-08-16 05:54:51 -0700

    task 8

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,10 +1,55 @@
 #!/usr/bin/python3
 """Starts a Flask web app"""
 from flask import Flask, render_template
-from models import States, storage
+from models import storage, State
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
+
+
+@app.route("/")
+def hello_world():
+    """Displays 'Hello HBNB!'"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb")
+def hbnb():
+    """Displays 'HBNB!'"""
+    return "HBNB"
+
+
+@app.route("/c/<text>")
+def cText(text):
+    """Returns a text"""
+    text = text.replace("_", " ")
+    return f"C {text}"
+
+
+@app.route("/python")
+@app.route("/python/<text>")
+def pythonText(text="is cool"):
+    """Returns a text"""
+    text = text.replace("_", " ")
+    return f"Python {text}"
+
+
+@app.route("/number/<int:n>")
+def number(n):
+    """Returns only if n is an integer"""
+    return f"{n} is a number"
+
+
+@app.route("/number_template/<int:n>")
+def numberTemplate(n):
+    """Returns an html page"""
+    return render_template("5-number.html", n=n)
+
+
+@app.route("/number_odd_or_even/<int:n>")
+def evenSo(n):
+    """Returns an html page depending on the parity of n"""
+    return render_template("6-number_odd_or_even.html", n=n)
 
 
 @app.teardown_appcontext
